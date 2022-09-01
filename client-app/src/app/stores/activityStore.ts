ba8cd7ca4c2059564039af7bbc86524de2ba91a7
Icevalley <asbjorn.isdal@gmail.com>
2022-09-01 12:10:00 +0200

    End of Section 9

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,4 +1,5 @@
 import { makeAutoObservable, runInAction } from "mobx";
+import { ObjectFlags } from "typescript";
 import agent from "../api/agent";
 import { Activity } from "../models/activity";
 
@@ -15,6 +16,15 @@ export default class ActivityStore {
 
     get activitiesByDate() {
         return Array.from(this.activityRegistry.values()).sort((a, b) => Date.parse(a.date) - Date.parse(b.date));
+    }
+
+    get groupedActivities() {
+        return Object.entries(this.activitiesByDate.reduce((activities, activity) => {
+            const date = activity.date;
+            activities[date] = activities[date] ? [...activities[date], activity] : [activity];
+            return activities;
+        }, {} as {[key: string] : Activity[]})
+        )
     }
 
     loadActivities = async () => {
