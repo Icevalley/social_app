168d73f36627ceaf8fb62074e60cc6f40cd649ec
Icevalley <asbjorn.isdal@gmail.com>
2022-09-01 10:41:27 +0200

    End of section 8

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -6,12 +6,12 @@ import { useStore } from "../../../app/stores/store";
 import ActivityList from "./ActivityList";
 
 export default observer (function ActivityDashboard() {
-
     const {activityStore} = useStore();
+    const {loadActivities, activityRegistry} = activityStore;
 
     useEffect(() => {
-      activityStore.loadActivities();
-    }, [activityStore])
+      if (activityRegistry.size <= 1) loadActivities();
+    }, [activityRegistry.size, loadActivities])
   
       if (activityStore.loadingInitial) return <LoadingComponent content='Loading App' />
 
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,15 +1,18 @@
 import { observer } from "mobx-react-lite";
 import React, { ChangeEvent, useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { Button, Form, Segment } from "semantic-ui-react";
 import LoadingComponent from "../../../app/layout/LoadingComponents";
 import { useStore } from "../../../app/stores/store";
+import { v4 as uuid } from 'uuid'
+import { useNavigate } from "react-router-dom";
 
 export default observer (function ActivityForm() {
 
     const {activityStore} = useStore();
     const {createActivity, updateActivity, loading, loadActivity, loadingInitial} = activityStore;
     const {id} = useParams();
+    const navigate = useNavigate();
 
     const [activity, setActivity] = useState({
         id: '',
@@ -26,7 +29,15 @@ export default observer (function ActivityForm() {
     }, [id, loadActivity])
 
     function handleSubmit() {
-        activity.id ? updateActivity(activity) : createActivity(activity);
+        if (activity.id.length === 0) {
+            let newActivity = {
+                ...activity,
+                id: uuid()
+            };
+            createActivity(newActivity).then(() => navigate(`/activities/${newActivity.id}`))
+        } else {
+            updateActivity(activity).then(() => navigate(`/activities/${activity.id}`))
+        }
     }
 
     function handleInputChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
@@ -46,7 +57,7 @@ export default observer (function ActivityForm() {
                 <Form.Input placeholder='City' value={activity.city} name='city' onChange={handleInputChange}/>
                 <Form.Input placeholder='Venue' value={activity.venue} name='venue' onChange={handleInputChange}/>
                 <Button loading={loading} floated='right' positive type='submit' content='submit' />
-                <Button floated='right' type='button' content='Cancel' />                   
+                <Button as={Link} to='/activities' floated='right' type='button' content='Cancel' />                   
             </Form>
         </Segment>)
 })
